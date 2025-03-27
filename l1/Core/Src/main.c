27915773df17faacc98dfa1748a8bb736058ead9
PimPTP <princesspim.ptp@gmail.com>
2025-03-27 18:17:04 +0700

    v5

--- l1/Core/Src/main.c
+++ l1/Core/Src/main.c
@@ -120,7 +120,7 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim3);
+  //HAL_TIM_Base_Start(&htim3);
   HAL_TIM_Base_Start_IT(&htim3);
   HAL_ADC_Start_DMA(&hadc1, ADCRawRead, 40);
 
